04e32f31346eea3403b53885a2dceb734966ef2d
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-10-06 22:46:34 +0300

    change test controller for personal tests

--- src/main/kotlin/com/rest_service/controller/TestController.kt
+++ src/main/kotlin/com/rest_service/controller/TestController.kt
@@ -1,25 +1,13 @@
 package com.rest_service.controller
 
-import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Produces
-import io.micronaut.security.annotation.Secured
-import javax.annotation.security.PermitAll
+import io.micronaut.http.annotation.Post
 
 @Controller("/test")
 class TestController {
-    @PermitAll
-    @Get("/permitAll")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun index(): String {
-        return "test permitt all"
-    }
 
-    @Secured("isAuthenticated()")
-    @Get("/secured")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun index2(): String {
-        return "test secured"
+    @Post("/")
+    fun create(data: String) {
+        println(data)
     }
 }
