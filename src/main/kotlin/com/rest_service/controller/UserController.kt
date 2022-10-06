19a35a12cb5aff728e78033fa96e31be4ecd1842
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-10-06 22:43:56 +0300

    implement user controller

---
+++ src/main/kotlin/com/rest_service/controller/UserController.kt
@@ -0,0 +1,28 @@
+package com.rest_service.controller
+
+import com.rest_service.dto.UserDTO
+import com.rest_service.service.UserService
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Produces
+import io.micronaut.security.annotation.Secured
+import reactor.core.publisher.Mono
+
+@Controller("/v1/user")
+class UserController(private val service: UserService) {
+    @Secured("isAuthenticated()")
+    @Get("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun get(): Mono<UserDTO> {
+        return service.get()
+    }
+
+    @Secured("isAuthenticated()")
+    @Post("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun create(): Mono<UserDTO> {
+        return service.create()
+    }
+}
---
+++ src/main/kotlin/com/rest_service/service/UserService.kt
@@ -0,0 +1,52 @@
+package com.rest_service.service
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.rest_service.domain.User
+import com.rest_service.dto.UserDTO
+import com.rest_service.exception.NotFoundException
+import com.rest_service.repository.UserRepository
+import io.micronaut.security.utils.SecurityService
+import jakarta.inject.Singleton
+import reactor.core.publisher.Mono
+import java.time.Instant
+import java.util.UUID
+
+@Singleton
+class UserService(private val userRepository: UserRepository, private val securityService: SecurityService) {
+    private val mapper = jacksonObjectMapper()
+
+    fun get(): Mono<UserDTO> {
+        val email = securityService.authentication.get().attributes["email"].toString()
+
+        return userRepository.findByEmail(email)
+            .switchIfEmpty(Mono.error(NotFoundException("User with email $email was not found.")))
+            .map {
+                mapper.convertValue(it, UserDTO::class.java)
+            }
+    }
+
+    fun create(): Mono<UserDTO> {
+        val email = securityService.authentication.get().attributes["email"].toString()
+
+        return userRepository.findByEmail(email)
+            .hasElement()
+            .flatMap {
+                if (it) {
+                    return@flatMap Mono.error(NotFoundException("User with email $email already exist."))
+                }
+
+                val now = Instant.now().toEpochMilli()
+
+                userRepository.save(
+                    User(
+                        UUID.randomUUID(),
+                        email,
+                        now,
+                        now
+                    )
+                ).map { user ->
+                    mapper.convertValue(user, UserDTO::class.java)
+                }
+            }
+    }
+}
