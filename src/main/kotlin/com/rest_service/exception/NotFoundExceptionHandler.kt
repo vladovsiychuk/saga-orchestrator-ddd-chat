07e51177fddc434488526dd89b6ab1cdd65809c3
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2022-10-12 23:51:22 +0300

    add status to error response

--- src/main/kotlin/com/rest_service/exception/NotFoundExceptionHandler.kt
+++ src/main/kotlin/com/rest_service/exception/NotFoundExceptionHandler.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Produces
 import io.micronaut.http.server.exceptions.ExceptionHandler
 import jakarta.inject.Singleton
@@ -16,7 +17,7 @@ class NotFoundExceptionHandler : ExceptionHandler<NotFoundException, HttpRespons
     private val mapper = jacksonObjectMapper()
 
     override fun handle(request: HttpRequest<*>, exception: NotFoundException): HttpResponse<*> {
-        val errorMap = mapOf("message" to exception.message)
+        val errorMap = mapOf("status" to HttpStatus.NOT_FOUND.code, "message" to exception.message)
 
         return HttpResponse.notFound<String>()
             .body(mapper.writeValueAsString(errorMap))
