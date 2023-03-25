b6b6ad52abefd729731e242242264757e40acde3
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-25 12:54:31 +0200

    entity without unique id cannot exist

--- src/main/kotlin/com/rest_service/domain/Member.kt
+++ src/main/kotlin/com/rest_service/domain/Member.kt
@@ -1,10 +1,13 @@
 package com.rest_service.domain
 
+import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
 import java.util.UUID
 
 @MappedEntity
 data class Member(
+    @field:Id
+    val id: UUID,
     val roomId: UUID,
     val userId: UUID,
     val joinedAt: Long,
