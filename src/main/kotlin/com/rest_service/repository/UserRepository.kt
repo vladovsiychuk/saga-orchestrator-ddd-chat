97ffe2a8edad75486b80df3ddf7b122b73719d29
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-25 11:45:08 +0200

    update to r2dbc

--- src/main/kotlin/com/rest_service/repository/UserRepository.kt
+++ src/main/kotlin/com/rest_service/repository/UserRepository.kt
@@ -1,15 +1,15 @@
 package com.rest_service.repository
 
 import com.rest_service.domain.User
-import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
-import io.micronaut.data.repository.reactive.ReactiveStreamsCrudRepository
+import io.micronaut.data.r2dbc.annotation.R2dbcRepository
+import io.micronaut.data.repository.reactive.ReactorCrudRepository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.UUID
 
-@JdbcRepository(dialect = Dialect.MYSQL)
-interface UserRepository : ReactiveStreamsCrudRepository<User, UUID> {
+@R2dbcRepository(dialect = Dialect.MYSQL)
+interface UserRepository : ReactorCrudRepository<User, UUID> {
     fun save(user: User): Mono<User>
     fun findByEmail(email: String): Mono<User>
 
