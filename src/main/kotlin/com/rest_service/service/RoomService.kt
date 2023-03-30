47bb8b0287f0ae8cd243ed6800bc8d178be6ef8b
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-30 22:01:21 +0300

    throw exception if user does not exist

--- src/main/kotlin/com/rest_service/service/RoomService.kt
+++ src/main/kotlin/com/rest_service/service/RoomService.kt
@@ -2,12 +2,14 @@ package com.rest_service.service
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.rest_service.dto.RoomDTO
+import com.rest_service.exception.NotFoundException
 import com.rest_service.repository.MemberRepository
 import com.rest_service.repository.RoomRepository
 import com.rest_service.repository.UserRepository
 import com.rest_service.util.SecurityUtil
 import jakarta.inject.Singleton
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Singleton
 class RoomService(
@@ -22,6 +24,7 @@ class RoomService(
         val email = securityUtil.getUserEmail()
 
         return userRepository.findByEmail(email)
+            .switchIfEmpty(Mono.error(NotFoundException("User with email $email was not found.")))
             .flux()
             .flatMap {
                 memberRepository.findByUserId(it.id)
