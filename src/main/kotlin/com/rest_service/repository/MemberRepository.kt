325f0ab72540cf7295f272f8bcd4e22847dedaef
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-30 21:48:53 +0300

    add necessary method

--- src/main/kotlin/com/rest_service/repository/MemberRepository.kt
+++ src/main/kotlin/com/rest_service/repository/MemberRepository.kt
@@ -4,8 +4,10 @@ import com.rest_service.domain.Member
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository
 import io.micronaut.data.repository.reactive.ReactorCrudRepository
+import reactor.core.publisher.Flux
 import java.util.UUID
 
 @R2dbcRepository(dialect = Dialect.MYSQL)
 interface MemberRepository : ReactorCrudRepository<Member, UUID> {
+    fun findByUserId(userId: UUID): Flux<Member>
 }
