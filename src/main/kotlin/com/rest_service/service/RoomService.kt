759ca2c7d5390645d352d815ce3580af33dfe82e
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-30 23:24:55 +0300

    implement the logic to create a new chat in the service

--- src/main/kotlin/com/rest_service/service/RoomService.kt
+++ src/main/kotlin/com/rest_service/service/RoomService.kt
@@ -1,6 +1,9 @@
 package com.rest_service.service
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.rest_service.command.RoomCommand
+import com.rest_service.domain.Member
+import com.rest_service.domain.Room
 import com.rest_service.dto.RoomDTO
 import com.rest_service.exception.NotFoundException
 import com.rest_service.repository.MemberRepository
@@ -8,6 +11,8 @@ import com.rest_service.repository.RoomRepository
 import com.rest_service.repository.UserRepository
 import com.rest_service.util.SecurityUtil
 import jakarta.inject.Singleton
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -19,6 +24,7 @@ class RoomService(
     private val roomRepository: RoomRepository,
 ) {
     private val mapper = jacksonObjectMapper()
+    private val logger: Logger = LoggerFactory.getLogger(RoomService::class.java)
 
     fun list(): Flux<RoomDTO> {
         val email = securityUtil.getUserEmail()
@@ -33,6 +39,39 @@ class RoomService(
                             .map { room ->
                                 mapper.convertValue(room, RoomDTO::class.java)
                             }
+                    }
+            }
+    }
+
+    fun create(command: RoomCommand): Mono<RoomDTO> {
+        val email = securityUtil.getUserEmail()
+
+        return Mono.zip(
+            userRepository.findByEmail(email),
+            userRepository.findById(command.userId)
+                .switchIfEmpty(Mono.error(NotFoundException("User with id ${command.userId} does not exist.")))
+        )
+            .flatMap { result ->
+                val currentUser = result.t1
+                val companionUser = result.t2
+
+                val room = Room(currentUser.id)
+
+                roomRepository.save(room)
+                    .flatMap { createdRoom ->
+                        val firstMember = Member(createdRoom.id, currentUser.id)
+                        val secondMember = Member(createdRoom.id, companionUser.id)
+
+                        Mono.zip(
+                            memberRepository.save(firstMember),
+                            memberRepository.save(secondMember)
+                        )
+                            .map {
+                                mapper.convertValue(createdRoom, RoomDTO::class.java)
+                            }
+                    }
+                    .doOnSuccess {
+                        logger.info("New chat between ${currentUser.email} and ${companionUser.email} created.")
                     }
             }
     }
