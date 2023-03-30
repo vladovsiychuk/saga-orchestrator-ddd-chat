88e3bd458b6b5bf9096ffd3b58565fe9a916a663
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-03-30 23:24:32 +0300

    save repository methods

--- src/main/kotlin/com/rest_service/repository/MemberRepository.kt
+++ src/main/kotlin/com/rest_service/repository/MemberRepository.kt
@@ -5,9 +5,14 @@ import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository
 import io.micronaut.data.repository.reactive.ReactorCrudRepository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.util.UUID
+import javax.transaction.Transactional
 
 @R2dbcRepository(dialect = Dialect.MYSQL)
 interface MemberRepository : ReactorCrudRepository<Member, UUID> {
     fun findByUserId(userId: UUID): Flux<Member>
+
+    @Transactional
+    fun save(member: Member): Mono<Member>
 }
--- src/main/kotlin/com/rest_service/repository/RoomRepository.kt
+++ src/main/kotlin/com/rest_service/repository/RoomRepository.kt
@@ -4,8 +4,12 @@ import com.rest_service.domain.Room
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository
 import io.micronaut.data.repository.reactive.ReactorCrudRepository
+import reactor.core.publisher.Mono
 import java.util.UUID
+import javax.transaction.Transactional
 
 @R2dbcRepository(dialect = Dialect.MYSQL)
 interface RoomRepository : ReactorCrudRepository<Room, UUID> {
+    @Transactional
+    fun save(room: Room): Mono<Room>
 }
