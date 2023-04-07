8ed6d2aa29e636bfacd04c6f280cf6525fdada1c
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-04-07 15:40:14 +0300

    create an interface for websocket service

--- src/main/kotlin/com/rest_service/websocket/WebSocketService.kt
+++ src/main/kotlin/com/rest_service/websocket/WebSocketService.kt
@@ -1,20 +1,7 @@
 package com.rest_service.websocket
 
-import io.micronaut.websocket.WebSocketBroadcaster
-import io.micronaut.websocket.WebSocketSession
-import jakarta.inject.Singleton
 import java.util.UUID
-import java.util.function.Predicate
 
-@Singleton
-class WebSocketService(private val broadcaster: WebSocketBroadcaster) {
-    fun sendMessageToUser(msg: String, userId: UUID) {
-        broadcaster.broadcastSync(msg, isValid(userId))
-    }
-
-    private fun isValid(userId: UUID): Predicate<WebSocketSession> {
-        return Predicate<WebSocketSession> {
-            userId == UUID.fromString(it.uriVariables.get("userId", String::class.java, null))
-        }
-    }
+interface WebSocketService {
+    fun sendMessageToUser(msg: String, userId: UUID)
 }
