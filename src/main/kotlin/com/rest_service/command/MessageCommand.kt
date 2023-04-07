3f0119a2aa0679b19d7b8647ed7b1f0b1455faee
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-04-07 17:05:24 +0300

    create message command

---
+++ src/main/kotlin/com/rest_service/command/MessageCommand.kt
@@ -0,0 +1,10 @@
+package com.rest_service.command
+
+import io.micronaut.core.annotation.Introspected
+import java.util.UUID
+
+@Introspected
+data class MessageCommand(
+    val roomId: UUID,
+    val content: String,
+)
