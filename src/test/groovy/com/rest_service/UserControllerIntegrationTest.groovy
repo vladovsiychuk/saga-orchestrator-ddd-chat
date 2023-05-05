b3b0d39755d9b3305bbc920068689d48d50195ca
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-05-05 18:14:52 +0300

    refactor user specs

--- src/test/groovy/com/rest_service/UserControllerIntegrationTest.groovy
+++ src/test/groovy/com/rest_service/UserControllerIntegrationTest.groovy
@@ -1,13 +1,8 @@
 package com.rest_service
 
-import com.rest_service.domain.Member
-import com.rest_service.domain.MessageEvent
-import com.rest_service.domain.User
+import com.rest_service.constant.UserConstant
 import com.rest_service.enums.LanguageEnum
-import com.rest_service.enums.MessageEventType
 import com.rest_service.enums.UserType
-import com.rest_service.repository.MemberRepository
-import com.rest_service.repository.MessageEventRepository
 import com.rest_service.repository.UserRepository
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
@@ -18,10 +13,8 @@ import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.reactor.http.client.ReactorHttpClient
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
-import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
-import spock.util.concurrent.PollingConditions
 
 @MicronautTest(transactional = false)
 @Requires(env = Environment.TEST)
@@ -32,124 +25,7 @@ class UserControllerIntegrationTest extends Specification {
     ReactorHttpClient client
 
     @Inject
-    @Shared
-    UserRepository userRepository
-
-    @Inject
-    @Shared
-    MemberRepository memberRepository
-
-    @Inject
-    @Shared
-    MessageEventRepository messageEventRepository
-
-    @Shared
-    String regular_user_1_token
-    @Shared
-    String user_2_token
-    @Shared
-    String user_3_token
-    @Shared
-    String translator_user_4
-    @Shared
-    UUID regular_user_1_id
-    @Shared
-    UUID translator_user_4_id
-
-    def conditions = new PollingConditions(timeout: 5, initialDelay: 0.5, factor: 1.25)
-
-    def setupSpec() {
-        regular_user_1_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJ1c2VyLTFAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.B7NnRHclfkrcOgK4HX8fqogY-oq3Hv1GrWTylDqOhrg"
-        user_2_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJ1c2VyLTJAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.2g43we2jYBkY15SmDvV4fz_bfgGvltY5F5udXUKRi2c"
-        user_3_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJ1c2VyLTNAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ._7KPCfylisPdIyvXcKAEjGKUPyKV_hfXKDsm4faF_4U"
-        translator_user_4 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJ1c2VyLTRAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.lRP3YeXmsEIw-xDwgaHgzyBB1Z3xowApEvq4W4VWsx8"
-
-        regular_user_1_id = UUID.fromString("50b1cccd-2cbd-459b-9b63-c97145b97e94")
-        translator_user_4_id = UUID.fromString("af7a16ae-cc5d-484e-8b1d-e89fd52d1150")
-        def companion_regular_user_5_id = UUID.fromString("9444016a-c6d3-4356-85e9-1b285c1a000f")
-        def companion_regular_user_6_id = UUID.fromString("385346a0-be02-4536-a5a1-681eea69eb83")
-
-        def room_id_1 = UUID.fromString("71ddb6fd-8640-4d23-a5b3-ae16e6945fd4")
-        def room_id_2 = UUID.fromString("3e878377-e0bc-423a-b307-3148e2f2c347")
-
-        def message_1_id = UUID.fromString("c2164861-792c-4a2f-a066-f0d2da067965")
-        def message_2_id = UUID.fromString("03cb5404-df57-4923-9740-80d12fa32d0e")
-
-        def regular_user_1_member_1 = new Member(UUID.randomUUID(), room_id_1, regular_user_1_id, 1)
-        def companion_regular_user_5_member = new Member(UUID.randomUUID(), room_id_1, companion_regular_user_5_id, 1)
-        def regular_user_1_member_2 = new Member(UUID.randomUUID(), room_id_2, regular_user_1_id, 1)
-        def companion_regular_user_6_member = new Member(UUID.randomUUID(), room_id_2, companion_regular_user_6_id, 1)
-
-        def regularUser = new User(regular_user_1_id, null, "user-1@gmail.com", null, LanguageEnum.ENGLISH, null, UserType.REGULAR_USER, 1, 1)
-        def translatorUser = new User(translator_user_4_id, null, "user-4@gmail.com", null, LanguageEnum.ENGLISH, [LanguageEnum.ENGLISH.toString(), LanguageEnum.UKRAINIAN.toString()], UserType.TRANSLATOR, 1, 1)
-        def companion_regular_user_5 = new User(companion_regular_user_5_id, null, "user-5@gmail.com", null, LanguageEnum.ENGLISH, null, UserType.REGULAR_USER, 1, 1)
-        def companion_regular_user_6 = new User(companion_regular_user_6_id, null, "user-6@gmail.com", null, LanguageEnum.ENGLISH, null, UserType.REGULAR_USER, 1, 1)
-
-        def message_1_message_new_event_1 = new MessageEvent(
-                UUID.randomUUID(),
-                message_1_id,
-                LanguageEnum.ENGLISH,
-                "new message",
-                room_id_1,
-                regular_user_1_id,
-                MessageEventType.MESSAGE_NEW,
-                1,
-        )
-
-        def message_1_message_read_event = new MessageEvent(
-                UUID.randomUUID(),
-                message_1_id,
-                null,
-                null,
-                null,
-                companion_regular_user_5_id,
-                MessageEventType.MESSAGE_READ,
-                2,
-        )
-
-        def message_1_message_translate_event = new MessageEvent(
-                UUID.randomUUID(),
-                message_1_id,
-                LanguageEnum.UKRAINIAN,
-                "translation text",
-                null,
-                translator_user_4_id,
-                MessageEventType.MESSAGE_TRANSLATE,
-                3
-        )
-
-        def message_1_message_new_event_2 = new MessageEvent(
-                UUID.randomUUID(),
-                message_2_id,
-                LanguageEnum.ENGLISH,
-                "new message",
-                room_id_2,
-                companion_regular_user_6_id,
-                MessageEventType.MESSAGE_NEW,
-                4,
-        )
-
-        memberRepository.saveAll([
-                regular_user_1_member_1,
-                companion_regular_user_5_member,
-                regular_user_1_member_2,
-                companion_regular_user_6_member,
-        ]).collectList().block()
-
-        userRepository.saveAll([
-                regularUser,
-                translatorUser,
-                companion_regular_user_5,
-                companion_regular_user_6,
-        ]).collectList().block()
-
-        messageEventRepository.saveAll([
-                message_1_message_new_event_1,
-                message_1_message_read_event,
-                message_1_message_translate_event,
-                message_1_message_new_event_2,
-        ]).collectList().block()
-    }
+    UserRepository repository
 
     @Unroll
     void "GET should return the DTO of current #expectedType user"() {
@@ -159,27 +35,27 @@ class UserControllerIntegrationTest extends Specification {
 
         then:
         response.body() == [
-                id                  : expectedUserId,
-                username            : null,
-                email               : expectedEmail,
-                avatar              : null,
-                primaryLanguage     : LanguageEnum.ENGLISH.toString(),
-                translationLanguages: expectedTranslationLanguages,
-                type                : expectedType,
-                dateCreated         : 1,
-                dateUpdated         : 1,
+            id                  : expectedUserId,
+            username            : expectedUsername,
+            email               : expectedEmail,
+            avatar              : expectedAvatar,
+            primaryLanguage     : expectedPrimaryLanguage.toString(),
+            translationLanguages: expectedTranslationLanguages,
+            type                : expectedType,
+            dateCreated         : 1,
+            dateUpdated         : 1,
         ]
 
         where:
-        expectedType                     | expectedTranslationLanguages                                         | expectedUserId                  | expectedEmail      | token
-        UserType.REGULAR_USER.toString() | null                                                                 | regular_user_1_id.toString()    | "user-1@gmail.com" | regular_user_1_token
-        UserType.TRANSLATOR.toString()   | [LanguageEnum.ENGLISH.toString(), LanguageEnum.UKRAINIAN.toString()] | translator_user_4_id.toString() | "user-4@gmail.com" | translator_user_4
+        expectedType                     | expectedTranslationLanguages                                         | expectedPrimaryLanguage | expectedUserId         | expectedEmail      | expectedUsername | expectedAvatar | token
+        UserType.REGULAR_USER.toString() | null                                                                 | LanguageEnum.UKRAINIAN  | UserConstant.USER_1_ID | "user-1@gmail.com" | 'username-1'     | 'avatar-1'     | UserConstant.USER_1_TOKEN
+        UserType.TRANSLATOR.toString()   | [LanguageEnum.ENGLISH.toString(), LanguageEnum.UKRAINIAN.toString()] | LanguageEnum.ENGLISH    | UserConstant.USER_4_ID | "user-4@gmail.com" | 'username-4'     | 'avatar-4'     | UserConstant.USER_4_TOKEN
     }
 
     @Unroll
     void "GET should return list of users when command is #command"() {
         when:
-        def request = HttpRequest.GET("/?$command").bearerAuth(regular_user_1_token)
+        def request = HttpRequest.GET("/?$command").bearerAuth(UserConstant.USER_1_TOKEN)
         def response = client.toBlocking().exchange(request, List)
 
         then:
@@ -187,13 +63,13 @@ class UserControllerIntegrationTest extends Specification {
 
         where:
         command        | expectedUsers
-        "query=user"   | ["user-1@gmail.com", "user-4@gmail.com", "user-5@gmail.com", "user-6@gmail.com"]
-        "roomLimit=30" | ["user-5@gmail.com", "user-6@gmail.com"]
+        "query=user"   | ["user-1@gmail.com", "user-2@gmail.com", "user-3@gmail.com", "user-4@gmail.com"]
+        "roomLimit=30" | ["user-3@gmail.com", "user-4@gmail.com"]
     }
 
     void "GET should return 404 response if the user doesn't exist"() {
         when:
-        def request = HttpRequest.GET("/currentUser").bearerAuth(user_2_token)
+        def request = HttpRequest.GET("/currentUser").bearerAuth(UserConstant.NOT_EXISTING_USER_TOKEN)
         client.toBlocking().exchange(request, Object)
 
         then:
@@ -204,20 +80,20 @@ class UserControllerIntegrationTest extends Specification {
     @Unroll
     void "POST should create a new #type user from authorization"() {
         when:
-        def request = HttpRequest.POST("/currentUser", command).bearerAuth(user_3_token)
+        def request = HttpRequest.POST("/currentUser", command).bearerAuth(UserConstant.NEW_USER_TOKEN)
         def response = client.toBlocking().exchange(request, Map)
 
         then:
         def body = response.body()
         body.username == null
-        body.email == "user-3@gmail.com"
+        body.email == "new-user@gmail.com"
         body.avatar == null
         body.primaryLanguage == LanguageEnum.ENGLISH.toString()
         body.translationLanguages?.sort() == command.translationLanguages?.sort()
         body.type == type
 
         cleanup:
-        userRepository.deleteAll().block()
+        repository.deleteById(UUID.fromString(response.body().id.toString())).block()
 
         where:
         type                             | command
@@ -228,7 +104,7 @@ class UserControllerIntegrationTest extends Specification {
     @Unroll
     void "POST should IllegalArgument exception if #reason"() {
         when:
-        def request = HttpRequest.POST("/currentUser", command).bearerAuth(user_3_token)
+        def request = HttpRequest.POST("/currentUser", command).bearerAuth(UserConstant.NEW_USER_TOKEN)
         client.toBlocking().exchange(request, Map)
 
         then:
@@ -241,12 +117,5 @@ class UserControllerIntegrationTest extends Specification {
         reason                                     | command                                                                                                                                               | expectedMessage
         "translator has less than 2 languages"     | [type: UserType.TRANSLATOR.toString(), primaryLanguage: LanguageEnum.ENGLISH.toString()]                                                              | "A translator user must have at least 1 translation language."
         "a regular user has translation languages" | [type: UserType.REGULAR_USER.toString(), primaryLanguage: LanguageEnum.ENGLISH.toString(), translationLanguages: [LanguageEnum.UKRAINIAN.toString()]] | "A regular user cannot have translation languages."
-    }
-
-    void "only cleanup"() {
-        cleanup:
-        messageEventRepository.deleteAll().block()
-        memberRepository.deleteAll().block()
-        userRepository.deleteAll().block()
     }
 }
--- src/test/groovy/com/rest_service/constant/UserConstant.groovy
+++ src/test/groovy/com/rest_service/constant/UserConstant.groovy
@@ -12,4 +12,8 @@ class UserConstant {
      * TRANSLATOR USER
      */
     static final USER_4_ID = "89a6cea2-52de-4295-942c-a0b7f485f561"
+    static final USER_4_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJ1c2VyLTRAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.lRP3YeXmsEIw-xDwgaHgzyBB1Z3xowApEvq4W4VWsx8"
+
+    static final NOT_EXISTING_USER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJub3QtZXhpc3RpbmdAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.bgc86EqsSYDcIubm0TkSd1Z4oMsvpfCv4bw0f76wVUQ"
+    static final NEW_USER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJuZXctdXNlckBnbWFpbC5jb20iLCJpYXQiOjE1MTYyMzkwMjJ9.Igr12iYGS0Of5cpJv4mwO3F3HXKndp29tahZXFvgCcw"
 }
